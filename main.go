c30dbfe81d4012a2674bbb5648f0527f7b2df661
namikaze-dev <usmansadik96@gmail.com>
2023-03-21 19:01:07 +0100

    Add support for writing to provided output file

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
 	"log"
 	"os"
 )
@@ -15,13 +17,18 @@ func main() {
 	var options struct {
 		i       bool
 		A, B, C int
+		o       string
 	}
 
 	flag.BoolVar(&options.i, "i", false, "case insensitize match")
 	flag.IntVar(&options.A, "A", 0, "print NUM lines of before match")
 	flag.IntVar(&options.B, "B", 0, "print NUM lines of after match")
 	flag.IntVar(&options.C, "C", 0, "print count of matches")
+	flag.StringVar(&options.o, "o", "", "file to write output")
 	flag.Parse()
+
+	// setup output destination
+	var output = setOuputDst(options.o)
 
 	if len(flag.Args()) == 0 {
 		infoLog.Println("grep: search key arg required")
@@ -37,9 +44,21 @@ func main() {
 		})
 
 		for _, l := range res {
-			infoLog.Println(l)
+			fmt.Fprintln(output, l)
 		}
 
 		return
 	}
+}
+
+func setOuputDst(fn string) io.Writer {
+	if fn != "" {
+		f, err := os.Create(fn)
+		if err != nil {
+			errLog.Fatalf("grep: %v", err)
+		}
+
+		return f
+	}
+	return os.Stdout
 }
