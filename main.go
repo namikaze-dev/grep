1df4a96ac4c069ef8e26a52489a9adf5e9c17a39
namikaze-dev <usmansadik96@gmail.com>
2023-03-21 12:19:13 +0100

    Fix parsing of redundant flag

--- main.go
+++ main.go
@@ -13,12 +13,11 @@ var (
 
 func main() {
 	var options struct {
-		r, i    bool
+		i    bool
 		A, B, C int
 	}
 
 	flag.BoolVar(&options.i, "i", false, "case insensitize match")
-	flag.BoolVar(&options.r, "r", false, "grep directory")
 	flag.IntVar(&options.A, "A", 0, "print NUM lines of before match")
 	flag.IntVar(&options.B, "B", 0, "print NUM lines of after match")
 	flag.IntVar(&options.C, "C", 0, "print count of matches")
